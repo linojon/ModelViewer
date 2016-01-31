2861822aaf9aaa473c303b3fe341d61a8133c17f
Jonathan Linowes <jonathan@parkerhill.com>
2016-01-31 15:14:53 -0500

    scale based on model extents

--- app/src/main/java/com/cardbookvr/modelviewer/MainActivity.java
+++ app/src/main/java/com/cardbookvr/modelviewer/MainActivity.java
@@ -1,18 +1,18 @@
 package com.cardbookvr.modelviewer;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cardbook.renderbox.IRenderBox;
 import com.cardbook.renderbox.RenderBox;
 import com.cardbook.renderbox.Transform;
-import com.cardbook.renderbox.math.MathUtils;
 import com.google.vrtoolkit.cardboard.CardboardActivity;
 import com.google.vrtoolkit.cardboard.CardboardView;
 
 public class MainActivity extends CardboardActivity implements IRenderBox {
     static String TAG = "MainActivity";
 
-    Transform model;
+    Transform modelRoot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +27,23 @@ public class MainActivity extends CardboardActivity implements IRenderBox {
 
     @Override
     public void setup() {
-        RenderBox.instance.mainLight.transform.setLocalPosition(0,0,-10);
-        model = new Transform()
-                .setLocalPosition(0, -2, -3)
-                .setLocalScale(0.01f, 0.01f, 0.01f)
-                .addComponent(new ModelObject(R.raw.teapot));
+        //RenderBox.instance.mainLight.transform.setLocalPosition(0,0,-10);
+        ModelObject model = new ModelObject(R.raw.skull);
+        Log.d(TAG, "extentsMin: " + model.extentsMin.toString() );
+        Log.d(TAG, "extentsMax: " + model.extentsMax.toString() );
+        modelRoot = new Transform()
+                .setLocalPosition(0, -2, -3);
+        new Transform()
+                .setParent(modelRoot)
+                .setLocalPosition(model.center())
+                .setLocalScale(model.normalScale())
+                .addComponent(model);
     }
 
     @Override
     public void preDraw() {
-        float[] hAngles = RenderBox.instance.headAngles;
-        model.setLocalRotation(hAngles[0] * MathUtils.radiansToDegrees, hAngles[1] * MathUtils.radiansToDegrees, hAngles[2] * MathUtils.radiansToDegrees);
+//        float[] hAngles = RenderBox.instance.headAngles;
+//        model.setLocalRotation(hAngles[0] * MathUtils.radiansToDegrees, hAngles[1] * MathUtils.radiansToDegrees, hAngles[2] * MathUtils.radiansToDegrees);
     }
 
     @Override
--- app/src/main/java/com/cardbookvr/modelviewer/ModelObject.java
+++ app/src/main/java/com/cardbookvr/modelviewer/ModelObject.java
@@ -7,6 +7,7 @@ import com.cardbook.renderbox.components.RenderObject;
 import com.cardbook.renderbox.materials.Material;
 import com.cardbook.renderbox.materials.SolidColorLightingMaterial;
 import com.cardbook.renderbox.materials.SolidColorMaterial;
+import com.cardbook.renderbox.math.Vector3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,8 +34,12 @@ public class ModelObject extends RenderObject {
     Vector<Float> vn = new Vector<Float>();
     Vector<Material> materials = null;
 
+    public Vector3 extentsMin, extentsMax;
+
     public ModelObject(int objFile) {
         super();
+        extentsMin = new Vector3(Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
+        extentsMax = new Vector3(Float.MIN_VALUE, Float.MIN_VALUE, Float.MIN_VALUE);
         parseObj(objFile);
         createSolidColorMaterial(true);
     }
@@ -89,7 +94,9 @@ public class ModelObject extends RenderObject {
         String[] tokens = line.split("[ ]+"); //split the line at the spaces
         int c = tokens.length;
         for (int i = 1; i < c; i++) { //add the vertex to the vertex array
-            v.add(Float.valueOf(tokens[i]));
+            Float value = Float.valueOf(tokens[i]);
+            v.add(value);
+            setExtents(i, value);
         }
     }
 
@@ -237,5 +244,40 @@ public class ModelObject extends RenderObject {
             triangles.add(polygon.get(i + 1));
         }
         return triangles;
+    }
+
+    private void setExtents(int coord, Float value) {
+        switch (coord) {
+            case 1:
+                if (value < extentsMin.x)
+                    extentsMin.x = value;
+                if (value > extentsMax.x)
+                    extentsMax.x = value;
+            case 2:
+                if (value < extentsMin.y)
+                    extentsMin.y = value;
+                if (value > extentsMax.y)
+                    extentsMax.y = value;
+            case 3:
+                if (value < extentsMin.z)
+                    extentsMin.z = value;
+                if (value > extentsMax.z)
+                    extentsMax.z = value;
+        }
+    }
+
+    public Vector3 center() {
+        float centerX = (extentsMax.x + extentsMin.x)/2.0f;
+        float centerY = (extentsMax.y + extentsMin.y)/2.0f;
+        float centerZ = (extentsMax.z + extentsMin.z)/2.0f;
+        return new Vector3(-centerX, -centerY, -centerZ);
+    }
+
+    public Vector3 normalScale() {
+        float sizeX = (extentsMax.x - extentsMin.x);
+        float sizeY = (extentsMax.y - extentsMin.y);
+        float sizeZ = (extentsMax.z - extentsMin.z);
+        float scalar = Math.max(sizeX, Math.max(sizeY, sizeZ)) * 10f;
+        return new Vector3(sizeX/scalar, sizeY/scalar, sizeZ/scalar);
     }
 }
